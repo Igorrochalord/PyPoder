732d44c96eb65e8d407d2cff67b6e518ba239b29
Igorrochalord <igorestagios@gmail.com>
2024-07-31 19:09:49 -0300

    breve att

---
+++ config/settings.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', 'YOUR_TELEGRAM_BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID', 'YOUR_TELEGRAM_CHAT_ID')
+NEWS_URL = 'https://example.com/news'
+SECTIONS = ['politics', 'business', 'sports', 'technology']
